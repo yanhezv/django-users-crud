7d0c5801f461a074e664b1af062140b8c2a6026a
Omar Villanueva Sotelo <yanhezv@gmail.com>
2026-01-15 15:32:06 -0500

    feat: add custom user model based on AbstractUser

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Custom user model extending Django's AbstractUser.
+    Ready for future extensions.
+    """
+
+    email = models.EmailField(unique=True)
+
+    REQUIRED_FIELDS = ["email"]
+
+    def __str__(self) -> str:
+        return self.username
