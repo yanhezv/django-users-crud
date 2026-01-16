fbe61ee39994549b68fdb91b099b2412079a27af
Omar Villanueva Sotelo <yanhezv@gmail.com>
2026-01-16 16:42:52 -0500

    feat: configure django admin for custom user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """
+    Admin configuration for custom User model.
+    """
+
+    model = User
+    list_display = ("id", "username", "email", "is_active", "is_staff")
+    list_filter = ("is_active", "is_staff", "is_superuser")
+    search_fields = ("username", "email")
+    ordering = ("id",)
